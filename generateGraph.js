e74c253d4214d5185de829fd9b861084acb1b9c0
micahstubbs <micah.stubbs@gmail.com>
2016-10-01 23:30:43 -0700

    add support-vector-machine

--- generateGraph.js
+++ generateGraph.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const jsonfile = require('jsonfile');
 
-const networkType = 'deep-residual-network';
+const networkType = 'support-vector-machine';
 const connectedness = 'fullyConnected';
 const inputFile = `${networkType}/layers.json`;
 const layers = JSON.parse(fs.readFileSync(inputFile, 'utf8'));
